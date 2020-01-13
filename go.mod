fc7e8c24528b51a51f24eaa50f2de42bf8d987b6
Cesar Alvarado <alvaradopcesar@gmail.com>
2020-01-13 12:20:08 -0500

    comments

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module arango_test
 
 go 1.13
 
-require github.com/arangodb/go-driver v0.0.0-20200107125107-2a2392e62f69 // indirect
+require github.com/arangodb/go-driver v0.0.0-20200107125107-2a2392e62f69
--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 
 // Book structure to collection book
 type Book struct {
-	Title   string `db="title"`
-	NoPages int32	`db="noPages"`
+	Title   string `json:"title"`
+	NoPages int32  `json:"noPages"`
 }
 
 func main() {
@@ -19,35 +19,35 @@ func main() {
 	conn, err := http.NewConnection(http.ConnectionConfig{
 		Endpoints: []string{"http://localhost:8529"},
 	})
-	PrintError(err,"error 01")
+	PrintError(err, "error 01")
 
 	c, err := driver.NewClient(driver.ClientConfig{
 		Connection: conn,
 	})
-	PrintError(err,"error 01")
+	PrintError(err, "error 01")
 
 	// Open "examples_books" database
 	var db driver.Database
 	foundDB, err := c.DatabaseExists(ctx, "examples_books")
-	PrintError(err,"error 02.exite databasen")
+	PrintError(err, "error 02.exite databasen")
 	if foundDB {
 		db, err = c.Database(ctx, "examples_books")
-		PrintError(err,"error 02.open database")
+		PrintError(err, "error 02.open database")
 	} else {
 		db, err = c.CreateDatabase(ctx, "examples_books", &driver.CreateDatabaseOptions{})
-		PrintError(err,"error 02.create database")
+		PrintError(err, "error 02.create database")
 	}
 
 	// Open "books" collection
 	var col driver.Collection
 	found, err := db.CollectionExists(ctx, "books")
-	PrintError(err,"error 03.exite collection")
+	PrintError(err, "error 03.exite collection")
 	if found {
 		col, err = db.Collection(ctx, "books")
-		PrintError(err,"error 03")
+		PrintError(err, "error 03")
 	} else {
 		col, err = db.CreateCollection(ctx, "books", &driver.CreateCollectionOptions{})
-		PrintError(err,"error 04")
+		PrintError(err, "error 04")
 	}
 
 	// Create document
@@ -56,21 +56,21 @@ func main() {
 		NoPages: 257,
 	}
 	meta, err := col.CreateDocument(ctx, book)
-	PrintError(err,"error 05")
+	PrintError(err, "error 05")
 
 	fmt.Println(meta)
 	fmt.Printf("Created document in collection '%s' in database '%s'\n", col.Name(), db.Name())
 
 	var doc Book
 	meta2, err := col.ReadDocument(ctx, meta.Key, &doc)
-	PrintError(err,"error 06")
+	PrintError(err, "error 06")
 
-	fmt.Println("leyendo el registro")
+	fmt.Println("reading from collection")
 	fmt.Println(meta2)
 
 }
 
-func PrintError(err error,msg string ){
+func PrintError(err error, msg string) {
 	if err != nil {
 		fmt.Println(msg, err.Error())
 	}
